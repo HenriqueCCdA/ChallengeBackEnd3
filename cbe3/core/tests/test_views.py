e2ed532c71f9e2202c746cb25aadba9822adb920
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-14 19:07:11 -0300

    Lendo o arquivo .csv.

--- cbe3/core/tests/test_views.py
+++ cbe3/core/tests/test_views.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from django.test import Client
 
 
-def test_status_code_home(client: Client):
-    resp = client.get('/')
+def test_read_csv_file(client: Client):
+    resp = client.get('/import/')
 
     assert resp.status_code == HTTPStatus.OK
