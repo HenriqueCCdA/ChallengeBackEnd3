1d4b9df762bcef79faca06f3e469b7e22fd2a613
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-19 21:09:06 -0300

    Mudar a rota .
    close #11

--- cbe3/core/tests/test_views.py
+++ cbe3/core/tests/test_views.py
@@ -1,20 +1,26 @@
 from http import HTTPStatus
 from unittest import mock
 
+from django.shortcuts import resolve_url
 from django.test import Client
 import pytest
 
 from cbe3.core.django_assertions import assert_template_use, assert_contains
 
 
-def test_read_csv_file(client: Client, db):
-    resp = client.get('/import/')
+@pytest.fixture
+def url():
+    return resolve_url('import_csv')
+
+
+def test_read_csv_file(client: Client, url, db):
+    resp = client.get(url)
 
     assert resp.status_code == HTTPStatus.OK
 
 
-def test_template_import_csv_file(client: Client, db):
-    resp = client.get('/import/')
+def test_template_import_csv_file(client: Client, url, db):
+    resp = client.get(url)
 
     assert_template_use(response=resp, template_name='import_csv.html')
 
@@ -27,6 +33,6 @@ def test_empty_csv(client, mocker, csv_all_valid):
     mocker.patch('builtins.open', mock_open)
 
     with open('file.csv') as f:
-        resp = client.post('/import/', {'csv-file': f})
+        resp = client.post('url', {'csv-file': f})
 
     assert_contains(resp, '<p>')
